b05c10b871b325bb0e4b21c7ef70567821984f91
Diego Gárate <diego.garate02@inacapmail.cl>
2025-08-18 22:19:09 -0400

    chore(core): ajustes menores en modelos

--- orders_inventory_api/core/models.py
+++ orders_inventory_api/core/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Product(models.Model):
+    sku = models.CharField(max_length=30,  unique=True)
+    name = models.CharField(max_length=120)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return f"{self.sku} - {self.name}"
